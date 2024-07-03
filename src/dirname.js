3a654b03a76dd3a60dff8c4b1c35d6fe79385d82
DiegoHerreraGre <diego.herrera.94@outlook.com>
2024-07-02 22:39:18 -0400

    Refactor JSON structure for improved readability and add global variables
    Restructured JSON data for better clarity. Introduced global variables for universal usage.
    
    - Enhances code readability and maintainability.
    - Introduces global variables for cross-file utilization.
    
    No references.

--- src/dirname.js
+++ src/dirname.js
@@ -5,3 +5,5 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default __dirname;
+
+/* Creación de variables globales para poder utilizar en cualquier archivo */
